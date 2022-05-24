216fcab142941c436af367b24464e5cc892a57e4
MATEO JAVIER AGUILAR CARRILLO <mateo.aguilar@elektra.com.mx>
2022-05-24 12:29:29 -0500

    prueba commit

--- lib/main.dart
+++ lib/main.dart
@@ -96,7 +96,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times:',
+              'Has apretado el botón:',
             ),
             Text(
               '$_counter',
