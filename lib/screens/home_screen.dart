ba7228aca2bdbae3b549e169250061cc789f3982
AgnosticSix <mateo_godfather@hotmail.com>
2022-06-14 12:45:27 -0500

    appbar added

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -5,8 +5,12 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("HomeScreen"),
+        elevation: 0.0,
+      ),
+      body: const Center(
         child: Text('HomeScreen'),
       ),
     );
