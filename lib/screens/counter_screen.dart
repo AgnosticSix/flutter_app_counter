d531e5daa679018b3c83cacc6eb53185d2ed6979
AgnosticSix <mateo_godfather@hotmail.com>
2022-07-02 16:06:10 -0500

    refactoring of floatingActionButtons

--- lib/screens/counter_screen.dart
+++ lib/screens/counter_screen.dart
@@ -8,7 +8,22 @@ class CounterScreen extends StatefulWidget {
 }
 
 class _CounterScreenState extends State<CounterScreen> {
-  int counter = 1;
+  int counter = 0;
+
+  void increase() {
+    counter++;
+    setState(() {});
+  }
+
+  void decrease() {
+    counter--;
+    setState(() {});
+  }
+
+  void restart() {
+    counter = 0;
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -29,13 +44,44 @@ class _CounterScreenState extends State<CounterScreen> {
         ),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      floatingActionButton: FloatingActionButton(
-        child: const Icon(Icons.add),
-        onPressed: () {
-          counter++;
-          setState(() {});
-        },
-      ),
+      floatingActionButton: CustomFloatingActions(
+          increaseFn: increase, decreaseFn: decrease, restartFn: restart),
+    );
+  }
+}
+
+class CustomFloatingActions extends StatelessWidget {
+  final Function increaseFn;
+  final Function decreaseFn;
+  final Function restartFn;
+
+  const CustomFloatingActions({
+    Key? key,
+    required this.increaseFn,
+    required this.decreaseFn,
+    required this.restartFn,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        FloatingActionButton(
+          child: const Icon(Icons.exposure_minus_1_outlined),
+          onPressed: () => decreaseFn(),
+        ),
+        //const SizedBox(width: 20),
+        FloatingActionButton(
+          child: const Icon(Icons.restart_alt_rounded),
+          onPressed: () => restartFn(),
+        ),
+        //const SizedBox(width: 20),
+        FloatingActionButton(
+          child: const Icon(Icons.exposure_plus_1_outlined),
+          onPressed: () => increaseFn(),
+        ),
+      ],
     );
   }
 }
